4a25c71fbf49bda7316f64102f6f2adecd06cf2b
zitj <1eyedea2eyedeas@gmail.com>
2020-11-10 14:19:36 +0100

    minor changes

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('We are connected!');
+class AgedPerson {
+    printAge(){
+        console.log(this.age);
+    }
+}
+
+
+class Person extends AgedPerson{
+    name = 'Aleksandar';
+
+constructor(){
+    super();
+    this.age = 27;
+}
+
+greet(){
+    console.log(`Hello I am ${this.name} and I am ${this.age} years old.`);
+}
+}
+
+// function Person(){
+//     this.age = 27;
+//     this.name = 'Aleksandar';
+//     this.greet = function(){
+//         console.log(`Hello I am ${this.name} and I am ${this.age} years old.`);
+//     };
+// }
+
+
+
+const person = new Person();
+
+person.greet();
+
+console.log(person.__proto__);
